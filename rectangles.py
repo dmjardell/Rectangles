3cab594ec9c7f6d1c2331ce394a954f61369dea3
dmjardell <david.mjardell@gmail.com>
2025-03-25 19:05:55 +0100

    Added collision detection between non-rotated rectangles

--- rectangles.py
+++ rectangles.py
@@ -1,35 +1,46 @@
 import tkinter as tk
 
-windowTitle = "Rectangles"
-windowSize = "800x600"
-gameArea = "800x600"
+WINDOWTITLE = "Rectangles"
+WINDOWSIZE = "800x600"
+GAMEAREA = "800x600"
 
 class Game:
-    def __init__(self, root, gameArea, gameTitle):
+    def __init__(self, root, GAMEAREA, gameTitle):
         self.root = root
-        self.root.geometry(gameArea)
+        self.root.geometry(GAMEAREA)
         self.root.title(gameTitle)
         self.canvas = tk.Canvas(
-                root, width=gameArea.split("x")[0],
-                height=gameArea.split("x")[1], bg="white")
+                root, width=GAMEAREA.split("x")[0],
+                height=GAMEAREA.split("x")[1], bg="white")
         self.canvas.pack()
+        self.objects = []
 
-        self.player = Rectangle(400, 300, 100, 100, "blue")
-        self.player.draw(self.canvas)
+    def start(self):
         self.root.bind("<KeyPress>", self.handleKeyPress)
 
+    def create_player(self, Rectangle):
+        self.player = Rectangle
+        self.player.draw(self.canvas)
+
+    def create_object(self, obj):
+        self.objects.append(obj)
+        obj.draw(self.canvas)
+
     def handleKeyPress(self, event):
-        moves = { 
+        moves = {
             "Up": (0, -10),
             "Down": (0, 10),
             "Left": (-10, 0),
             "Right": (10, 0)
             }
         if event.keysym in moves:
-            self.player.move(self.canvas, *moves[event.keysym])
+            if not (self.player.is_out_of_bounds(
+                GAMEAREA, *moves[event.keysym])
+                    or self.player.is_colliding_rect(*moves[event.keysym],
+                                                     self.objects[0])):
 
-
-
+                self.player.move(self.canvas, *moves[event.keysym])
+                print(self.objects[0].x)
 
 
 class Rectangle:
@@ -50,9 +61,30 @@ class Rectangle:
                     self.x, self.y, self.x + self.width,
                     self.y + self.height, fill=self.color)
     def move(self, canvas, dx, dy):
+        self.x += dx
+        self.y += dy
         canvas.move(self.canvas_id, dx, dy)
+
+    def is_out_of_bounds(self, GAMEAREA, dx, dy):
+        width, height = map(int, GAMEAREA.split("x"))
+        if(self.x + self.width + dx >= width or self.y + self.height + dy >= height
+           or self.x + dx <= 0 or self.y + dy <= 0):
+            return True
+        return False
+
+    def is_colliding_rect(self, dx, dy, rect):
+        if not (self.x + self.width + dx < rect.x
+                or rect.x + rect.width < self.x + dx
+                or self.y + self.height + dy < rect.y
+                or rect.y + rect.height < self.y + dy):
+            return True
+        return False
+
 
 
 root = tk.Tk()
-game = Game(root, gameArea, windowTitle)
+game = Game(root, GAMEAREA, WINDOWTITLE)
+game.create_player(Rectangle(400, 300, 100, 100, "blue"))
+game.create_object(Rectangle(200, 50, 100, 100, "red"))
+game.start()
 root.mainloop()
